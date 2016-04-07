45c1f3d91b467f54eac6151d435171048913a131
Kevin_Feng <Kevin_Feng@139.com>
2016-04-07 09:21:38 +0800

    第二章——冯如杯 启程

---
+++ PCE相机/PCE相机/cameraPhoto.swift
@@ -0,0 +1,13 @@
+//
+//  cameraPhoto.swift
+//  PCE相机
+//
+//  Created by Kevin_Feng on 16/4/7.
+//  Copyright © 2016年 Kevin_Feng. All rights reserved.
+//
+
+import UIKit
+
+class cameraPhoto: NSObject {
+
+}
---
+++ PCE相机/PCE相机Tests/PCE__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PCE__Tests.swift
+//  PCE相机Tests
+//
+//  Created by Kevin_Feng on 16/4/7.
+//  Copyright © 2016年 Kevin_Feng. All rights reserved.
+//
+
+import XCTest
+@testable import PCE__
+
+class PCE__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
